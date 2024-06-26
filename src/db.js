d46db6f11d9557556fbe2d96fa3c62b168962877
Sofiia Kozachenko <sofakozachenko2004@gmail.com>
2024-06-26 20:17:51 +0300

    fix: DB name fixed

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'Sofia_IAM',
   host: 'ship-db.cnaeciq0o9w4.ap-northeast-1.rds.amazonaws.com',
-  database: 'ship-db',
+  database: 'postgres',
   password: 'Adm007Adm00',
   port: 5432,
   ssl: {
